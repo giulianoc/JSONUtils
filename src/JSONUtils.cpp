10f3bc4b5e7b3c2c612772710b0c04664091bfb6
giulianoc <giulianocatrambone@gmail.com>
2025-10-18 09:17:36 +0200

    code review

--- src/JSONUtils.cpp
+++ src/JSONUtils.cpp
@@ -1,5 +1,5 @@
 #include "JSONUtils.h"
-#include "spdlog/spdlog.h"
+#include <format>
 #include <fstream>
 #include <regex>
 
@@ -9,407 +9,13 @@ extern char **_environ;
 extern char **environ;
 #endif
 
-bool JSONUtils::isMetadataPresent(json root, string field)
-{
-	if (root == nullptr)
-		return false;
-	else
-		return root.contains(field);
-}
 
-bool JSONUtils::isNull(json root, string field)
-{
-	if (root == nullptr)
-	{
-		string errorMessage = std::format(
-			"JSONUtils::isNull, root is null"
-			", field: {}",
-			field
-		);
-		SPDLOG_ERROR(errorMessage);
-
-		throw runtime_error(errorMessage);
-	}
-
-	return root[field].is_null();
-}
-
-string JSONUtils::asString(json root, string field, string defaultValue, bool notFoundAsException)
-{
-	if (notFoundAsException && !isMetadataPresent(root, field))
-	{
-		string errorMessage = std::format(
-			"Field not found"
-			", field: {}",
-			field
-		);
-
-		throw JsonFieldNotFound(errorMessage);
-	}
-
-	if (root == nullptr)
-		return defaultValue;
-
-	try
-	{
-		if (field == "")
-		{
-			if (root.type() == json::value_t::number_integer)
-				return to_string(root.template get<int>());
-			else if (root.type() == json::value_t::number_float)
-				return to_string(root.template get<float>());
-			else if (root.type() == json::value_t::object)
-				return toString(root);
-			else if (root.type() == json::value_t::array)
-				return toString(root);
-			else
-				return root.template get<string>();
-		}
-		else
-		{
-			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
-				return defaultValue;
-			if (root.at(field).type() == json::value_t::number_integer || root.at(field).type() == json::value_t::number_float ||
-				root.at(field).type() == json::value_t::boolean)
-				return to_string(root.at(field));
-			else
-				return root.at(field);
-		}
-	}
-	catch (json::out_of_range &e)
-	{
-		return defaultValue;
-	}
-}
-
-int JSONUtils::asInt(json root, string field, int defaultValue, bool notFoundAsException)
-{
-	if (notFoundAsException && !isMetadataPresent(root, field))
-	{
-		string errorMessage = std::format(
-			"Field not found"
-			", field: {}",
-			field
-		);
-
-		throw JsonFieldNotFound(errorMessage);
-	}
-
-	if (root == nullptr)
-		return defaultValue;
-
-	try
-	{
-		if (field == "")
-		{
-			if (root.type() == json::value_t::string)
-			{
-				try
-				{
-					return strtol(asString(root, "", "0").c_str(), nullptr, 10);
-				}
-				catch (exception &e)
-				{
-					return defaultValue;
-				}
-			}
-			else
-				return root.template get<int>();
-		}
-		else
-		{
-			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
-				return defaultValue;
-			if (root.at(field).type() == json::value_t::string)
-			{
-				try
-				{
-					return strtol(asString(root, field, "0").c_str(), nullptr, 10);
-				}
-				catch (exception &e)
-				{
-					return defaultValue;
-				}
-			}
-			else
-				return root.at(field);
-		}
-	}
-	catch (json::out_of_range &e)
-	{
-		return defaultValue;
-	}
-}
-
-int64_t JSONUtils::asInt64(json root, string field, int64_t defaultValue, bool notFoundAsException)
-{
-	if (notFoundAsException && !isMetadataPresent(root, field))
-	{
-		string errorMessage = std::format(
-			"Field not found"
-			", field: {}",
-			field
-		);
-
-		throw JsonFieldNotFound(errorMessage);
-	}
-
-	if (root == nullptr)
-		return defaultValue;
-
-	try
-	{
-		if (field == "")
-		{
-			if (root.type() == json::value_t::string)
-			{
-				try
-				{
-					return stoll(asString(root, "", "0").c_str());
-				}
-				catch (exception &e)
-				{
-					return defaultValue;
-				}
-			}
-			else
-				return root.template get<int64_t>();
-		}
-		else
-		{
-			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
-				return defaultValue;
-			if (root.at(field).type() == json::value_t::string)
-			{
-				try
-				{
-					return stoll(asString(root, field, "0").c_str());
-				}
-				catch (exception &e)
-				{
-					return defaultValue;
-				}
-			}
-			else
-				return root.at(field);
-		}
-	}
-	catch (json::out_of_range &e)
-	{
-		return defaultValue;
-	}
-}
-
-uint64_t JSONUtils::asUint64(json root, string field, int64_t defaultValue, bool notFoundAsException)
-{
-	if (notFoundAsException && !isMetadataPresent(root, field))
-	{
-		string errorMessage = std::format(
-			"Field not found"
-			", field: {}",
-			field
-		);
-
-		throw JsonFieldNotFound(errorMessage);
-	}
-
-	if (root == nullptr)
-		return defaultValue;
-
-	try
-	{
-		if (field == "")
-		{
-			if (root.type() == json::value_t::string)
-			{
-				try
-				{
-					return stoull(asString(root, "", "0").c_str());
-				}
-				catch (exception &e)
-				{
-					return defaultValue;
-				}
-			}
-			else
-				return root.template get<uint64_t>();
-		}
-		else
-		{
-			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
-				return defaultValue;
-			if (root.at(field).type() == json::value_t::string)
-			{
-				try
-				{
-					return stoull(asString(root, field, "0").c_str());
-				}
-				catch (exception &e)
-				{
-					return defaultValue;
-				}
-			}
-			else
-				return root.at(field);
-		}
-	}
-	catch (json::out_of_range &e)
-	{
-		return defaultValue;
-	}
-}
-
-double JSONUtils::asDouble(json root, string field, double defaultValue, bool notFoundAsException)
-{
-	if (notFoundAsException && !isMetadataPresent(root, field))
-	{
-		string errorMessage = std::format(
-			"Field not found"
-			", field: {}",
-			field
-		);
-
-		throw JsonFieldNotFound(errorMessage);
-	}
-
-	if (root == nullptr)
-		return defaultValue;
-
-	try
-	{
-		if (field == "")
-		{
-			if (root.type() == json::value_t::string)
-			{
-				try
-				{
-					return stod(asString(root, "", "0"), nullptr);
-				}
-				catch (exception &e)
-				{
-					return defaultValue;
-				}
-			}
-			else
-				return root.template get<double>();
-		}
-		else
-		{
-			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
-				return defaultValue;
-			if (root.at(field).type() == json::value_t::string)
-			{
-				try
-				{
-					return stod(asString(root, field, "0"), nullptr);
-				}
-				catch (exception &e)
-				{
-					return defaultValue;
-				}
-			}
-			else
-				return root.at(field);
-		}
-	}
-	catch (json::out_of_range &e)
-	{
-		return defaultValue;
-	}
-}
-
-bool JSONUtils::asBool(json root, string field, bool defaultValue, bool notFoundAsException)
-{
-	if (notFoundAsException && !isMetadataPresent(root, field))
-	{
-		string errorMessage = std::format(
-			"Field not found"
-			", field: {}",
-			field
-		);
-
-		throw JsonFieldNotFound(errorMessage);
-	}
-
-	if (root == nullptr)
-		return defaultValue;
-
-	try
-	{
-		if (field == "")
-		{
-			if (root.type() == json::value_t::string)
-			{
-				string sTrue = "true";
-
-				bool isEqual = asString(root, "", "").length() != sTrue.length()
-								   ? false
-								   : equal(
-										 asString(root, "", "").begin(), asString(root, "", "").end(), sTrue.begin(),
-										 [](int c1, int c2) { return toupper(c1) == toupper(c2); }
-									 );
-
-				return isEqual ? true : false;
-			}
-			else
-				return root.template get<bool>();
-		}
-		else
-		{
-			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
-				return defaultValue;
-			if (root.at(field).type() == json::value_t::string)
-			{
-				string sTrue = "true";
-
-				bool isEqual = asString(root, field, "").length() != sTrue.length()
-								   ? false
-								   : equal(
-										 asString(root, field, "").begin(), asString(root, field, "").end(), sTrue.begin(),
-										 [](int c1, int c2) { return toupper(c1) == toupper(c2); }
-									 );
-
-				return isEqual ? true : false;
-			}
-			else
-				return root.at(field);
-		}
-	}
-	catch (json::out_of_range &e)
-	{
-		return defaultValue;
-	}
-}
-
-json JSONUtils::asJson(json root, string field, json defaultValue, bool notFoundAsException)
-{
-	bool isPresent = isMetadataPresent(root, field);
-
-	if (notFoundAsException && !isPresent)
-	{
-		string errorMessage = std::format(
-			"Field not found"
-			", field: {}",
-			field
-		);
-
-		throw JsonFieldNotFound(errorMessage);
-	}
-
-	// è presente oppure non è presente ma non deve dare eccezione
-
-	if (!isPresent)
-		return defaultValue;
-	else
-		return root[field];
-}
-
-json JSONUtils::toJson(string j, bool warningIfError)
+json JSONUtils::toJson(const string_view &j, bool warningIfError)
 {
 	try
 	{
-		if (j == "")
-			return json();
+		if (j.empty())
+			return {};
 		else
 			return json::parse(j);
 	}
@@ -430,7 +36,33 @@ json JSONUtils::toJson(string j, bool warningIfError)
 	}
 }
 
-json JSONUtils::toJson(vector<int32_t> v)
+ordered_json JSONUtils::toOrderedJson(const string_view &j, bool warningIfError)
+{
+	try
+	{
+		if (j.empty())
+			return {};
+		else
+			return ordered_json::parse(j);
+	}
+	catch (json::parse_error &ex)
+	{
+		string errorMessage = std::format(
+			"failed to parse the json"
+			", json: {}"
+			", at byte: {}",
+			j, ex.byte
+		);
+		if (warningIfError)
+			SPDLOG_WARN(errorMessage);
+		else
+			SPDLOG_ERROR(errorMessage);
+
+		throw runtime_error(errorMessage);
+	}
+}
+
+json JSONUtils::toJson(const vector<int32_t> &v)
 {
 	json root = json::array();
 	for (int32_t i : v)
@@ -438,34 +70,74 @@ json JSONUtils::toJson(vector<int32_t> v)
 	return root;
 }
 
-json JSONUtils::toJson(vector<string> v)
+ordered_json JSONUtils::toOrderedJson(const vector<int32_t> &v)
 {
-	json root = json::array();
-	for (string i : v)
+	ordered_json root = ordered_json::array();
+	for (int32_t i : v)
 		root.push_back(i);
 	return root;
 }
 
-string JSONUtils::toString(json root, int indent)
+json JSONUtils::toJson(const vector<string> &v)
 {
+	json root = json::array();
+	for (const string& i : v)
+		root.push_back(i);
+	return root;
+}
+
+ordered_json JSONUtils::toOrderedJson(const vector<string> &v)
+{
+	ordered_json root = ordered_json::array();
+	for (const string& i : v)
+		root.push_back(i);
+	return root;
+}
+
+json JSONUtils::loadConfigurationFile(const string_view &configurationPathName, const string_view &environmentPrefix)
+{
+
+#ifdef BOOTSERVICE_DEBUG_LOG
+#ifdef _WIN32
+	ofstream of("C:\\bootservice.log", ofstream::app);
+#else
+	ofstream of("/tmp/bootservice.log", ofstream::app);
+#endif
+	of << "loadConfigurationFile..." << endl;
+#endif
+
+	string sConfigurationFile;
 	try
 	{
-		if (root == nullptr)
-			return "null";
+		ifstream configurationFile(configurationPathName, ifstream::binary);
+		stringstream buffer;
+		buffer << configurationFile.rdbuf();
+		if (environmentPrefix.empty())
+			sConfigurationFile = buffer.str();
 		else
-			return root.dump(indent, ' ', true);
-	}
-	catch (const json::type_error &e)
-	{
-		throw runtime_error(e.what());
-	}
-	/*
-		Json::StreamWriterBuilder wbuilder;
-		wbuilder.settings_["emitUTF8"] = true;
-		wbuilder.settings_["indentation"] = "";
+			sConfigurationFile = applyEnvironmentToConfiguration(buffer.str(), environmentPrefix);
 
-		return Json::writeString(wbuilder, joValueRoot);
-	*/
+		json configurationRoot = json::parse(
+			sConfigurationFile,
+			nullptr, // callback
+			true,	 // allow exceptions
+			true	 // ignore_comments
+		);
+
+		return configurationRoot;
+	}
+	catch (exception &e)
+	{
+#ifdef BOOTSERVICE_DEBUG_LOG
+#ifdef _WIN32
+		ofstream of("C:\\bootservice.log", ofstream::app);
+#else
+		ofstream of("/tmp/bootservice.log", ofstream::app);
+#endif
+		of << "loadConfigurationFile failed, configurationPathName: " << configurationPathName << ", exception: " << e << endl;
+#endif
+		throw;
+	}
 }
 
 // Rimuove commenti singola riga e multi-riga
@@ -501,53 +173,7 @@ string JSONUtils::json5ToJson(const string &json5)
 	return cleaned;
 }
 
-json JSONUtils::loadConfigurationFile(string configurationPathName, string environmentPrefix)
-{
-
-#ifdef BOOTSERVICE_DEBUG_LOG
-#ifdef _WIN32
-	ofstream of("C:\\bootservice.log", ofstream::app);
-#else
-	ofstream of("/tmp/bootservice.log", ofstream::app);
-#endif
-	of << "loadConfigurationFile..." << endl;
-#endif
-
-	string sConfigurationFile;
-	try
-	{
-		ifstream configurationFile(configurationPathName, ifstream::binary);
-		stringstream buffer;
-		buffer << configurationFile.rdbuf();
-		if (environmentPrefix == "")
-			sConfigurationFile = buffer.str();
-		else
-			sConfigurationFile = applyEnvironmentToConfiguration(buffer.str(), environmentPrefix);
-
-		json configurationRoot = json::parse(
-			sConfigurationFile,
-			nullptr, // callback
-			true,	 // allow exceptions
-			true	 // ignore_comments
-		);
-
-		return configurationRoot;
-	}
-	catch (exception &e)
-	{
-#ifdef BOOTSERVICE_DEBUG_LOG
-#ifdef _WIN32
-		ofstream of("C:\\bootservice.log", ofstream::app);
-#else
-		ofstream of("/tmp/bootservice.log", ofstream::app);
-#endif
-		of << "loadConfigurationFile failed, configurationPathName: " << configurationPathName << ", exception: " << e << endl;
-#endif
-		throw;
-	}
-}
-
-string JSONUtils::applyEnvironmentToConfiguration(string configuration, string environmentPrefix)
+string JSONUtils::applyEnvironmentToConfiguration(string configuration, const string_view &environmentPrefix)
 {
 #ifdef _WIN32
 	char **s = _environ;
@@ -572,14 +198,14 @@ string JSONUtils::applyEnvironmentToConfiguration(string configuration, string e
 #endif
 		if (envVariable.starts_with(environmentPrefix))
 		{
-			size_t endOfVarName = envVariable.find("=");
+			size_t endOfVarName = envVariable.find('=');
 			if (endOfVarName == string::npos)
 				continue;
 
 			envNumber++;
 
 			// sarebbe \$\{CATRAMMSGUIAPPS_PATH\}
-			string envLabel = std::format("\\$\\{{{}\\}}", envVariable.substr(0, endOfVarName));
+			string envLabel = std::format(R"(\$\{{{}\}})", envVariable.substr(0, endOfVarName));
 			string envValue = envVariable.substr(endOfVarName + 1);
 #ifdef BOOTSERVICE_DEBUG_LOG
 			of << "ENV " << envLabel << ": " << envValue << endl;
--- src/JSONUtils.h
+++ src/JSONUtils.h
@@ -11,10 +11,12 @@
  * Created on March 29, 2018, 6:27 AM
  */
 
-#ifndef JSONUtils_h
-#define JSONUtils_h
+#pragma once
+
+#include <utility>
 
 #include "nlohmann/json.hpp"
+#include "spdlog/spdlog.h"
 
 using namespace std;
 
@@ -26,47 +28,430 @@ struct JsonFieldNotFound : public exception
 {
 	string _errorMessage;
 
-	JsonFieldNotFound(string errorMessage) { _errorMessage = errorMessage; }
+	explicit JsonFieldNotFound(string_view errorMessage):_errorMessage(errorMessage){};
 
-	char const *what() const throw() { return _errorMessage.c_str(); };
+	[[nodiscard]] char const *what() const noexcept override { return _errorMessage.c_str(); };
 };
 
 class JSONUtils
 {
-
   public:
-	static bool isMetadataPresent(json root, string field);
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static bool isMetadataPresent(const J &root, string_view field)
+	{
+		if (root == nullptr)
+			return false;
+		return root.contains(field);
+	}
 
-	static bool isNull(json root, string field);
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static bool isNull(const J &root, string_view field)
+	{
+		if (root == nullptr)
+		{
+			string errorMessage = std::format(
+				"JSONUtils::isNull, root is null"
+				", field: {}",
+				field
+			);
 
-	static string asString(json root, string field = "", string defaultValue = "", bool notFoundAsException = false);
+			throw runtime_error(errorMessage);
+		}
 
-	static int asInt(json root, string field = "", int defaultValue = 0, bool notFoundAsException = false);
+		return root[field].is_null();
+	}
 
-	static int64_t asInt64(json root, string field = "", int64_t defaultValue = 0, bool notFoundAsException = false);
-	static uint64_t asUint64(json root, string field = "", int64_t defaultValue = 0, bool notFoundAsException = false);
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static string asString(const J &root, string_view field = "", string_view defaultValue = "", bool notFoundAsException = false)
+	{
+		if (notFoundAsException && !isMetadataPresent(root, field))
+		{
+			string errorMessage = std::format(
+				"Field not found"
+				", field: {}",
+				field
+			);
 
-	static double asDouble(json root, string field = "", double defaultValue = 0.0, bool notFoundAsException = false);
+			throw JsonFieldNotFound(errorMessage);
+		}
 
-	static bool asBool(json root, string field, bool defaultValue = false, bool notFoundAsException = false);
+		if (root == nullptr)
+			return string(defaultValue);
 
-	static json asJson(json root, string field, json defaultValue = json(), bool notFoundAsException = false);
+		try
+		{
+			if (field.empty())
+			{
+				if (root.type() == json::value_t::number_integer)
+					return to_string(root.template get<int>());
+				if (root.type() == json::value_t::number_float)
+					return to_string(root.template get<float>());
+				if (root.type() == json::value_t::object)
+					return toString(root);
+				if (root.type() == json::value_t::array)
+					return toString(root);
+				return root.template get<string>();
+			}
 
-	static json toJson(string json, bool warningIfError = false);
+			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
+				return string(defaultValue);
+			if (root.at(field).type() == json::value_t::number_integer || root.at(field).type() == json::value_t::number_float ||
+				root.at(field).type() == json::value_t::boolean)
+				return to_string(root.at(field));
+			return root.at(field);
+		}
+		catch (json::out_of_range &e)
+		{
+			return string(defaultValue);
+		}
+	}
 
-	static string toString(json joValueRoot, int indent = -1);
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static int asInt(const J &root, string_view field = "", int defaultValue = 0, bool notFoundAsException = false)
+	{
+		if (notFoundAsException && !isMetadataPresent(root, field))
+		{
+			string errorMessage = std::format(
+				"Field not found"
+				", field: {}",
+				field
+			);
 
-	static json toJson(vector<int32_t> v);
-	static json toJson(vector<string> v);
+			throw JsonFieldNotFound(errorMessage);
+		}
+
+		if (root == nullptr)
+			return defaultValue;
+
+		try
+		{
+			if (field.empty())
+			{
+				if (root.type() == json::value_t::string)
+				{
+					try
+					{
+						return strtol(asString(root, "", "0").c_str(), nullptr, 10);
+					}
+					catch (exception &e)
+					{
+						return defaultValue;
+					}
+				}
+				return root.template get<int>();
+			}
+
+			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
+				return defaultValue;
+			if (root.at(field).type() == json::value_t::string)
+			{
+				try
+				{
+					return strtol(asString(root, field, "0").c_str(), nullptr, 10);
+				}
+				catch (exception &e)
+				{
+					return defaultValue;
+				}
+			}
+			return root.at(field);
+		}
+		catch (json::out_of_range &e)
+		{
+			return defaultValue;
+		}
+	}
+
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static int64_t asInt64(const J &root, string_view field = "", int64_t defaultValue = 0, bool notFoundAsException = false)
+	{
+		if (notFoundAsException && !isMetadataPresent(root, field))
+		{
+			string errorMessage = std::format(
+				"Field not found"
+				", field: {}",
+				field
+			);
+
+			throw JsonFieldNotFound(errorMessage);
+		}
+
+		if (root == nullptr)
+			return defaultValue;
+
+		try
+		{
+			if (field.empty())
+			{
+				if (root.type() == json::value_t::string)
+				{
+					try
+					{
+						return stoll(asString(root, "", "0").c_str());
+					}
+					catch (exception &e)
+					{
+						return defaultValue;
+					}
+				}
+				return root.template get<int64_t>();
+			}
+
+			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
+				return defaultValue;
+			if (root.at(field).type() == json::value_t::string)
+			{
+				try
+				{
+					return stoll(asString(root, field, "0").c_str());
+				}
+				catch (exception &e)
+				{
+					return defaultValue;
+				}
+			}
+			return root.at(field);
+		}
+		catch (json::out_of_range &e)
+		{
+			return defaultValue;
+		}
+	}
+
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static uint64_t asUint64(const J &root, string_view field = "", int64_t defaultValue = 0, bool notFoundAsException = false)
+	{
+		if (notFoundAsException && !isMetadataPresent(root, field))
+		{
+			string errorMessage = std::format(
+				"Field not found"
+				", field: {}",
+				field
+			);
+
+			throw JsonFieldNotFound(errorMessage);
+		}
+
+		if (root == nullptr)
+			return defaultValue;
+
+		try
+		{
+			if (field.empty())
+			{
+				if (root.type() == json::value_t::string)
+				{
+					try
+					{
+						return stoull(asString(root, "", "0").c_str());
+					}
+					catch (exception &e)
+					{
+						return defaultValue;
+					}
+				}
+				return root.template get<uint64_t>();
+			}
+
+			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
+				return defaultValue;
+			if (root.at(field).type() == json::value_t::string)
+			{
+				try
+				{
+					return stoull(asString(root, field, "0").c_str());
+				}
+				catch (exception &e)
+				{
+					return defaultValue;
+				}
+			}
+			return root.at(field);
+		}
+		catch (json::out_of_range &e)
+		{
+			return defaultValue;
+		}
+	}
+
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static double asDouble(const J &root, string_view field = "", double defaultValue = 0.0, bool notFoundAsException = false)
+	{
+		if (notFoundAsException && !isMetadataPresent(root, field))
+		{
+			string errorMessage = std::format(
+				"Field not found"
+				", field: {}",
+				field
+			);
+
+			throw JsonFieldNotFound(errorMessage);
+		}
+
+		if (root == nullptr)
+			return defaultValue;
+
+		try
+		{
+			if (field.empty())
+			{
+				if (root.type() == json::value_t::string)
+				{
+					try
+					{
+						return stod(asString(root, "", "0"), nullptr);
+					}
+					catch (exception &e)
+					{
+						return defaultValue;
+					}
+				}
+				return root.template get<double>();
+			}
+
+			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
+				return defaultValue;
+			if (root.at(field).type() == json::value_t::string)
+			{
+				try
+				{
+					return stod(asString(root, field, "0"), nullptr);
+				}
+				catch (exception &e)
+				{
+					return defaultValue;
+				}
+			}
+			return root.at(field);
+		}
+		catch (json::out_of_range &e)
+		{
+			return defaultValue;
+		}
+	}
+
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static bool asBool(const J &root, string_view field, bool defaultValue = false, bool notFoundAsException = false)
+	{
+		if (notFoundAsException && !isMetadataPresent(root, field))
+		{
+			string errorMessage = std::format(
+				"Field not found"
+				", field: {}",
+				field
+			);
+
+			throw JsonFieldNotFound(errorMessage);
+		}
+
+		if (root == nullptr)
+			return defaultValue;
+
+		try
+		{
+			if (field.empty())
+			{
+				if (root.type() == json::value_t::string)
+				{
+					string sTrue = "true";
+
+					bool isEqual = asString(root, "", "").length() != sTrue.length()
+									   ? false
+									   : equal(
+											 asString(root, "", "").begin(), asString(root, "", "").end(), sTrue.begin(),
+											 [](int c1, int c2) { return toupper(c1) == toupper(c2); }
+										 );
+
+					return isEqual ? true : false;
+				}
+				return root.template get<bool>();
+			}
+
+			if (!JSONUtils::isMetadataPresent(root, field) || JSONUtils::isNull(root, field))
+				return defaultValue;
+			if (root.at(field).type() == json::value_t::string)
+			{
+				string sTrue = "true";
+
+				bool isEqual = asString(root, field, "").length() != sTrue.length()
+								   ? false
+								   : equal(
+										 asString(root, field, "").begin(), asString(root, field, "").end(), sTrue.begin(),
+										 [](int c1, int c2) { return toupper(c1) == toupper(c2); }
+									 );
+
+				return isEqual ? true : false;
+			}
+			return root.at(field);
+		}
+		catch (json::out_of_range &e)
+		{
+			return defaultValue;
+		}
+	}
+
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static J asJson(const J &root, string_view field, J defaultValue = J(), bool notFoundAsException = false)
+	{
+		bool isPresent = isMetadataPresent(root, field);
+
+		if (notFoundAsException && !isPresent)
+		{
+			string errorMessage = std::format(
+				"Field not found"
+				", field: {}",
+				field
+			);
+
+			throw JsonFieldNotFound(errorMessage);
+		}
+
+		// è presente oppure non è presente ma non deve dare eccezione
+		if (!isPresent)
+			return defaultValue;
+		return root[field];
+	}
+
+	static json toJson(const string_view &json, bool warningIfError = false);
+	static ordered_json toOrderedJson(const string_view &json, bool warningIfError = false);
+
+	template <typename J>
+	requires is_same_v<J, json> || is_same_v<J, ordered_json>
+	static string toString(const J &root, int indent = -1)
+	{
+		try
+		{
+			if (root == nullptr)
+				return "null";
+			else
+				return root.dump(indent, ' ', true);
+		}
+		catch (const json::type_error &e)
+		{
+			throw runtime_error(e.what());
+		}
+	}
+
+	static json toJson(const vector<int32_t> &v);
+	static ordered_json toOrderedJson(const vector<int32_t> &v);
+	static json toJson(const vector<string> &v);
+	static ordered_json toOrderedJson(const vector<string> &v);
 
 	static string json5ToJson(const string &json5);
-	static json loadConfigurationFile(string configurationPathName, string environmentPrefix);
+	static json loadConfigurationFile(const string_view &configurationPathName, const string_view &environmentPrefix);
 
   private:
 	static string json5_removeComments(const string &input);
 	static string json5_removeTrailingCommas(const string &input);
 	static string json5_quoteUnquotedKeys(const string &input);
-	static string applyEnvironmentToConfiguration(string configuration, string environmentPrefix);
+	static string applyEnvironmentToConfiguration(string configuration, const string_view &environmentPrefix);
 };
-
-#endif
